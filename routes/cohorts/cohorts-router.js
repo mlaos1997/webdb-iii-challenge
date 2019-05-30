e2bd449d75cbe88395b130993ac7b8b15afd9da9
Marcelo Laos <mlaos1997@gmail.com>
2019-05-30 00:10:14 -0400

    need to complete routes for both cohorts and students

--- routes/cohorts/cohorts-router.js
+++ routes/cohorts/cohorts-router.js
@@ -7,13 +7,39 @@ const router = express.Router();
 router.get('/', async(req, res) => {
     try {
         const cohorts = await cohortsDb.get();
-        if(Object.keys(cohorts) === 0) {
-            res.status(400).json({ message: 'Could not retrieve cohorts from database' })
+        if (Object.keys(cohorts) === 0) {
+            res
+                .status(400)
+                .json({message: 'Could not retrieve cohorts from database'})
         }
-        res.status(200).json(cohorts);
-    } catch(err) {
-        res.status(500).json({ err });
+        res
+            .status(200)
+            .json(cohorts);
+    } catch (err) {
+        res
+            .status(500)
+            .json({err});
     };
 });
+
+router.get('/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const cohort = await cohortsDb.getById(id);
+        if (cohort) {
+            res
+                .status(200)
+                .json(cohort);
+        } else {
+            res
+                .status(404)
+                .json({message: 'The cohort with the specified id could not be found'});
+        }
+    } catch (err) {
+        res
+            .status(500)
+            .json({err});
+    }
+})
 
 module.exports = router;
--- routes/students/students-router.js
+++ routes/students/students-router.js
@@ -8,7 +8,7 @@ router.get('/', async(req, res) => {
     try {
         const students = await studentsDb.get();
         if(Object.keys(students) === 0) {
-            res.status(400).json({ message: 'Could not retrieve students from database' })
+            res.status(404).json({ message: 'Could not retrieve students from database' })
         }
         res.status(200).json(students);
     } catch(err) {
