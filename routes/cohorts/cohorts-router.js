56868329f0caec7f245e556189b545388c104212
Marcelo Laos <mlaos1997@gmail.com>
2019-05-30 20:48:25 -0400

    cohorts routes complete

--- routes/cohorts/cohorts-router.js
+++ routes/cohorts/cohorts-router.js
@@ -40,6 +40,69 @@ router.get('/:id', async(req, res) => {
             .status(500)
             .json({err});
     }
-})
+});
+
+router.post('/', async(req, res) => {
+    const newCohort = req.body;
+    if (newCohort.name) {
+        try {
+            const cohort = await cohortsDb.insert(newCohort);
+            res
+                .status(200)
+                .json(cohort);
+        } catch (err) {
+            res
+                .status(500)
+                .json({err})
+        };
+    } else {
+        res
+            .status(400)
+            .json({message: 'Please provide name of cohort'})
+    }
+});
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const deleteCohort = await cohortsDb.remove(id);
+        if (req.body.length > 0) {
+            res
+                .status(404)
+                .json({message: 'cohort not found'})
+        } else {
+            res
+                .status(201)
+                .json({message: 'cohort deleted'})
+        }
+    } catch (err) {
+        res
+            .status(500)
+            .json({err})
+    }
+});
+
+router.put('/:id', async(req, res) => {
+    const updatedCohort = req.body;
+    const {id} = req.params;
+    if (updatedCohort.name) {
+        try {
+            const cohort = await cohortsDb.update(id, updatedCohort);
+            if (cohort) {
+                res
+                    .status(200)
+                    .json(cohort);
+            } else {
+                res
+                    .status(404)
+                    .json({message: 'cohort not found in database'})
+            }
+        } catch (err) {
+            res
+                .status(500)
+                .json({err});
+        }
+    }
+});
 
 module.exports = router;
